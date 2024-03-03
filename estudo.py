95cd0e564b4d076909f1d5f585ecaf2a4972e677
Victor Nicacio <victorcnicacio@hotmail.com>
2024-03-03 20:15:15 -0300

    Aula 1 a 4

--- estudo.py
+++ estudo.py
@@ -1,7 +1,26 @@
 print("Olá Mundo")
 
-nome = "Victor Nicacio"
-idade = 24
+x = ["Carro", 1, 4, True] #List
+x[1] = x[1] + x[2] #Os valores em uma Lista podem ser reatribuidos
+print(x) #Printa todos valores dentro da Lista
+print(x[0]) #Printa o valor na posição 0 da Lista
 
-print(nome)
-print(idade)
+x = ("Carro", 1, 4, False) #Tupla
+#Os valores em uma Tupla NÃO PODEM ser reatribuidos
+print(x) #Printa todos valores dentro da Tupla
+print(x[3]) #Printa o valor na posição 3 da Tupla
+
+x = { #Dict
+    "Nome":"Victor",
+    "Faculdade":"FATEC-Id",
+    "Curso":"ADS"
+}
+x["Nome"] = "Victor Nicacio" #Os valores das chaves em uma Dict podem ser reatribuidos
+print(x) #Printa todas as chaves/valores dentro da Dict
+print(x["Faculdade"]) #Printa o valor da chave especificada
+
+x = {1, 2, 3, 1, 3} #Set
+print(x) #Ao printar um set, valores repetidos serão descartados no print
+
+print("Valor: " + str(x)) #O comando "str()" foi usado para fazer uma conversão do Set em string,
+#visto que ao tentar concatenar uma string com o set, é gerado um erro, necessitando assim da conversão.
