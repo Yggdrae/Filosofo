808ab08736825bf36c239574bb4e020d8255395b
Victor Nicacio <victorcnicacio@hotmail.com>
2024-03-06 20:11:14 -0300

    Input e Loops

---
+++ Input.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('cls')
+
+nome = input("Entre com seu nome: ")
+
+print("\n\n"+nome)
---
+++ Loops.py
@@ -0,0 +1,16 @@
+import os
+os.system('cls')
+
+mtg = ["Dimir", "Selesnya",  "Gruul", "Orzhov"]
+
+"""for x in mtg:
+    if(x == "Gruul"):
+        break
+    print(x)
+"""
+    
+i = 0
+while i<=3:
+    print(mtg[i])
+    i+=1
+print("Fim do programa.")
