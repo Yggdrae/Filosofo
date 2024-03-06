41418d8e851e2dd0077f2ae090efe44ea7c344f7
Victor Nicacio <victorcnicacio@hotmail.com>
2024-03-06 00:23:54 -0300

    if/else

---
+++ If.py
@@ -0,0 +1,16 @@
+a = 10
+b = 11
+op="-"
+
+if op=="+":
+    res=a+b
+elif op=="-":
+    res=a-b
+elif op=="/":
+    res=a/b
+elif op=="*":
+    res=a*b
+else:
+    print("Operador invalido")
+
+print(res)
