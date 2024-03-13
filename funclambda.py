7a0e7aa329bb80c92a253bc9795c64958590995f
Victor Nicacio <victorcnicacio@hotmail.com>
2024-03-12 23:43:37 -0300

    remove comments

--- funclambda.py
+++ funclambda.py
@@ -1,7 +1,7 @@
-#soma = lambda a,b: a+b
-#print(soma(int(input("Digite o valor de A: ")), int(input("Digite o valor de B: "))))
+soma = lambda a,b: a+b
+print(soma(int(input("Digite o valor de A: ")), int(input("Digite o valor de B: "))))
 
-#print((lambda a,b,c:a*b+c)(int(input("Digite o valor de A: ")), int(input("Digite o valor de B: ")), int(input("Digite o valor de C: "))))
+print((lambda a,b,c:a*b+c)(int(input("Digite o valor de A: ")), int(input("Digite o valor de B: ")), int(input("Digite o valor de C: "))))
 
 r = lambda x,func:x+func(x)
 res = r(2, lambda x:x*x)
