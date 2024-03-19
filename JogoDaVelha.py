4d2a14422887439c45ce44eafdd39eb337e17cdf
Victor Nicacio <victorcnicacio@hotmail.com>
2024-03-19 00:00:44 -0300

    Criado variaveis globais e print do jogo

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -2,3 +2,27 @@ import os
 import random
 from colorama import Fore, Back, Style
 
+jogarnovamente = "s"
+jogadas = 0
+quemJoga=2
+maxJogadas=9
+vit = False
+velha = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
+
+def attjogo():
+    global velha
+    global jogadas
+    os.system('cls')
+    print("    0   1   2")
+    print("0:  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2])
+    print("   -----------")
+    print("0:  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2])
+    print("   -----------")
+    print("0:  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2])
+    print("Jogadas: " + Fore.GREEN + str(jogadas) + Fore.RESET)
+
+attjogo()
